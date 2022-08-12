6a9f8c3d542c0f758efd41d9e7fb1baa33040f55
Arjunkrishna17 <107336846+Arjunkrishna17@users.noreply.github.com>
2022-08-12 15:06:20 +0530

    sorting error rectification

--- src/App.js
+++ src/App.js
@@ -3,38 +3,33 @@
 import Header from "./component/Header";
 import Addrating from "./component/Addrating";
 import Moviecard from "./component/Movie_card";
-import moment from 'moment'
-import "./App.css"
-import {v4 as uuidv4 } from 'uuid'
+import moment from "moment";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 import React, { useState, useEffect } from "react";
 
 /*------------------------Import Section------------------------------------------*/
 
-
-
 /*------------------------Global Variable Section----------------------------------*/
 
 const LOC_STOR_KEY = "movie";
 const retrieveData = JSON.parse(localStorage.getItem(LOC_STOR_KEY));
-let date = moment().format('lll');
+let date = moment().format("lll");
 /*------------------------Global Variable Section----------------------------------*/
-
-
 
 /*------------------------Function to render all component-----------------------------*/
 
 function App() {
-  
   const [movieData, setMovieData] = useState([]);
 
   // movie handler fn receive argument from addRating component
   const movieHandler = (movie) => {
-    setMovieData([...movieData,{id: uuidv4(),createdAt:date,...movie}]);
-    console.log(movie)
+    setMovieData([...movieData, { id: uuidv4(), createdAt: date, ...movie }]);
+    console.log(movie);
   };
 
-//retrieve data from localStorage on visit page
+  //retrieve data from localStorage on visit page
   useEffect(() => {
     setMovieData(retrieveData);
   }, []);
@@ -55,32 +50,25 @@ function App() {
   };
 
   // sort card accoring to name/ rating
-
-  movieData.sort((a,b) =>{
-  return (a.rating - b.rating)
-  })
-
-  
+  if (movieData.length > 1) {
+    movieData.sort((a, b) => {
+      return a.rating - b.rating;
+    });
+  }
 
   return (
     <div class="container">
-      <div class="row">    
-         <div class="">
-            <Header />
-            <Addrating movieHandler={movieHandler} />
-         </div>
+      <div class="row">
+        <div class="">
+          <Header />
+          <Addrating movieHandler={movieHandler} />
+        </div>
 
-         <div class="col-4 cln2">
-            <Moviecard movieData={movieData} deleteHandler={deleteHandler} />
-         </div>
-     </div>
-  </div>
-    
-    
-        
-     
-      
- 
+        <div class="col-4 cln2">
+          <Moviecard movieData={movieData} deleteHandler={deleteHandler} />
+        </div>
+      </div>
+    </div>
   );
 }
 
